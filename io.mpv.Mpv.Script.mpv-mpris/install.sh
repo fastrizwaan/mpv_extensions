cdce657ab20e0cb40ed65c414a984c93f9bdd2c8
Asif Ali Rizvan <fast.rizwaan@gmail.com>
2022-08-24 10:56:27 +0530

    added mpv-autosub and mpv-video-cutter

--- io.mpv.Mpv.Script.mpv-mpris/install.sh
+++
@@ -1,1 +0,0 @@
-flatpak-builder --force-clean build-dir/ io.mpv.Mpv.Script.mpv-mpris.yml
---
+++ io.mpv.Mpv.Script.mpv-video-cutter-linux/install.sh
@@ -0,0 +1,1 @@
+flatpak-builder --install --user --force-clean build-dir/ io.mpv.Mpv.Script.mpv-video-cutter-linux.yml
